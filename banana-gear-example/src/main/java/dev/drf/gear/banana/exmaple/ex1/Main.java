66c11dd212f5e16c277486e987400f462244bf6d
drfdev <drafff@gmail.com>
2020-05-31 17:23:38 +0300

    add hello world example

---
+++ banana-gear-example/src/main/java/dev/drf/gear/banana/exmaple/ex1/Main.java
@@ -0,0 +1,8 @@
+package dev.drf.gear.banana.exmaple.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.run();
+    }
+}
